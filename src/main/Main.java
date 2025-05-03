96a6908ff2314d766ac433da3051e2be8af0d8b5
Zenffer <jeoliveros@my.cspc.edu.ph>
2025-05-03 21:01:40 +0800

    accmngmnt

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,7 +4,7 @@ import ui.LoginWindow;
 
 public class Main {
     public static void main(String[] args) {
-        // Ensure GUI is created on the Event Dispatch Thread
+        // Ensure GUI is created on the Event Dispatch Thread hi
         javax.swing.SwingUtilities.invokeLater(() -> new LoginWindow());
     }
 }
